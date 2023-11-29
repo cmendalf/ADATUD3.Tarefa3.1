cf9b95501bf89960d09a53fa4e8694e9453ffe39
node <node@10.0.2.15>
2023-11-29 22:34:38 +0100

    <sd<dg<sdg

--- src/clases/HibernateUtil.java
+++ src/clases/HibernateUtil.java
@@ -1,12 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package clases;
 
-import org.hibernate.cfg.Configuration;
-import org.hibernate.SessionFactory;
+import com.mysql.cj.xdevapi.SessionFactory;
+import javax.security.auth.login.Configuration;
+import org.hibernate.*;
+
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
--- src/clases/Operaciones.java
+++ src/clases/Operaciones.java
@@ -1,16 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clases;
+
+import org.hibernate.Session;
 
 /**
  *
  * @author node
  */
+/*
+a) Crea un método que reciba o nome dun departamento e o insira na base de datos.
+b) Crea un método que reciba un obxecto empregado e o insira na base de datos.
+c) Crea un método que reciba o nss dun empregado e visualice os seus datos.
+d) Crea un método que reciba o número dun departamento e o borre da base de datos.
+e) Crea un método que reciba o número dun departamento e un nome, e cambie o nome do departamento
+polo nome do parámetro introducido.
+ */
 public class Operaciones {
+
+    private static Session sesion;
     //Abrir y cerrar sesión 
-    
-    
+
+    public static void abrirSesion() {
+        sesion = HibernateUtil.getSessionFactory().openSession();
+    }
+
+    public static void cerrarSesion() {
+        sesion.close();
+        HibernateUtil.getSessionFactory().close();
+    }
+
+    public static void addDepartmento(int idDepa, String nombreDepa) {
+        Departamento depa = new Departamento(idDepa, nombreDepa);
+        sesion.beginTransaction();
+        sesion.save(depa);
+        sesion.getTransaction().commit();
+    }
+
+    public static void addEmpregado(Empregado emp) {
+        sesion.beginTransaction();
+        sesion.save(emp);
+        sesion.getTransaction().commit();
+    }
+
+    public static String verEmpregado(String idEmp) {
+        StringBuilder sb = new StringBuilder();
+        sesion.beginTransaction();
+        Empregado empregado = (Empregado) sesion.get(Empregado.class, idEmp);
+        sesion.getTransaction().commit();
+        return sb.toString(); 
+    }
+    //
+
 }
